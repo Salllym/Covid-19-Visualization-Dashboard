5a2e1d504fba2f1726bf19781ecb855cad50d316
Cole Barnes <colebarnes7@gmail.com>
2022-06-23 12:06:42 -0500

    API routes for geojson files and adding coordinates to covid data

---
+++ geoJSON/static/js/logic.js
@@ -0,0 +1,37 @@
+// Set up using local files (works currently)
+// d3.json("/state_boundaries.json").then(function (state_data) {
+//     state_features = state_data.features;
+//     d3.json("/cleaned_covid_case.geojson").then(function (covid_data) {
+//         covid_features = covid_data.features;
+//         for (var i = 0; i < covid_features.length; i++){
+//             covid_features[i].geometry = state_features[i].geometry;
+//         };
+//         console.log(covid_data);
+//     });
+//     d3.json("/cleaned_covid_vaccine.geojson").then(function (vaccine_data) {
+//         vaccine_features = vaccine_data.features;
+//         for (var i = 0; i < vaccine_features.length; i++){
+//             vaccine_features[i].geometry = state_features[i].geometry;
+//         };
+//         console.log(vaccine_data);
+//     });
+// });
+
+// Set up for using API routes (not currently working)
+d3.json("/state_boundaries/geoJSON").then(function (state_data) {
+    state_features = state_data.features;
+    d3.json("/cleaned_covid_case/geoJSON").then(function (covid_data) {
+        covid_features = covid_data.features;
+        for (var i = 0; i < covid_features.length; i++){
+            covid_features[i].geometry = state_features[i].geometry;
+        };
+        console.log(covid_data);
+    });
+    d3.json("/cleaned_covid_vaccine/geoJSON").then(function (vaccine_data) {
+        vaccine_features = vaccine_data.features;
+        for (var i = 0; i < vaccine_features.length; i++){
+            vaccine_features[i].geometry = state_features[i].geometry;
+        };
+        console.log(vaccine_data);
+    });
+});
