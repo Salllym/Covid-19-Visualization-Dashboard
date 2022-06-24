0f01df073e7a8fe2ace5dae7c168ca42b48aa36f
Cole Barnes <colebarnes7@gmail.com>
2022-06-24 16:01:35 -0500

    maps working, added basic popup box and coloring

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,6 +7,7 @@ d3.json("/state_boundaries/geoJSON").then(function (state_data) {
             covid_features[i].geometry = state_features[i].geometry;
         };
         console.log(covid_data);
+        createCovidMap(covid_data);
     });
     d3.json("/cleaned_covid_vaccine/geoJSON").then(function (vaccine_data) {
         vaccine_features = vaccine_data.features;
@@ -14,5 +15,84 @@ d3.json("/state_boundaries/geoJSON").then(function (state_data) {
             vaccine_features[i].geometry = state_features[i].geometry;
         };
         console.log(vaccine_data);
+        createVaccineMap(vaccine_data);
     });
 });
+
+function createCovidMap(data) {
+    var map = L.map('map', {
+        center: [37.8, -96],
+        zoom: 4,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.geoJson(data, {
+        style: style_covid,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h3>${feature['properties']['State/Territory']}</h3>`);
+        }
+    }).addTo(map);
+};
+
+function createVaccineMap(data) {
+    var map = L.map('map2', {
+        center: [37.8, -96],
+        zoom: 4,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.geoJson(data, {
+        style: style_vaccine,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h3>${feature['properties']['State/Territory']}</h3>`);
+        }
+    }).addTo(map);
+};
+
+// Function to decide color for covid map
+function getCovidColor(x) {
+    return x > 450 ? '#800026' :
+           x > 300 ? '#E31A1C' :
+           x > 150 ? '#FD8D3C' :
+                     '#FED976';
+};
+
+// Function to style the covid_map
+function style_covid(feature) {
+    return {
+        fillColor: getCovidColor(feature['properties']['7-Day Cases Rate per 100000']),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    }; 
+}
+
+// Function to decide color for vaccine map
+function getVaccineColor(x) {
+    return x > 250000 ? '#800026' :
+           x > 200000 ? '#E31A1C' :
+           x > 150000 ? '#FD8D3C' :
+                     '#FED976';
+};
+
+// Function to style the vaccine_map
+function style_vaccine(feature) {
+    return {
+        fillColor: getVaccineColor(feature['properties']['Doses Distributed per 100K']),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    }; 
+}
