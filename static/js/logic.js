6b82bc3bc5fcd10c1500e737a13177f00f356822
Cole Barnes <colebarnes7@gmail.com>
2022-06-25 11:57:46 -0500

    added layer control and mouse events to leaflet covid map

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,10 +19,95 @@ d3.json("/state_boundaries/geoJSON").then(function (state_data) {
     });
 });
 
+// function createCovidCases(covid_data) {
+//     L.geoJson(data, {
+//         style: styleCovidCases,
+//         onEachFeature: function (feature, layer) {
+//             layer.bindPopup(`<h4>${feature['properties']['State/Territory']}</h4><hr><p>New Cases: ${feature['properties']['New Cases']}</p>
+//                 <p>Cases in Last 7 Days: ${feature['properties']['Cases in Last 7 Days']}</p><p>7-Day Cases Rate per 100000: ${feature['properties']['7-Day Cases Rate per 100000']}</p>
+//                 <p>Total Cases: ${feature['properties']['Total Cases']}</p><p>Case Rate per 100000: ${feature['properties']['Case Rate per 100000']}</p>`, 
+//                 {closeButton: false, offset: L.point(-20, -20)});
+//             layer.on('mouseover', function() {
+//                 layer.openPopup();
+//             });
+//             layer.on('mouseout', function() {
+//                 layer.closePopup();
+//             });
+//             layer.on('click', function(event) {
+//                 map.fitBounds(event.target.getBounds());
+//             });
+//         }
+//     });
+// }
+
+// function createCovidDeaths(covid_data) {
+//     L.geoJson(covid_data, {
+//         style: styleCovidDeaths,
+//         onEachFeature: function (feature, layer) {
+//             layer.bindPopup(`<h4>${feature['properties']['State/Territory']}</h4><hr><p>New Deaths: ${feature['properties']['New Deaths']}</p>
+//                 <p>Deaths in Last 7 Days: ${feature['properties']['Deaths in Last 7 Days']}</p><p>7-Day Death Rate per 100000: ${feature['properties']['7-Day Death Rate per 100000']}</p>
+//                 <p>Total Deaths: ${feature['properties']['Total Deaths']}</p><p>Death Rate per 100000: ${feature['properties']['Death Rate per 100000']}</p>`, 
+//                 {closeButton: false, offset: L.point(-20, -20)});
+//             layer.on('mouseover', function() {
+//                 layer.openPopup();
+//             });
+//             layer.on('mouseout', function() {
+//                 layer.closePopup();
+//             });
+//             layer.on('click', function(event) {
+//                 map.fitBounds(event.target.getBounds());
+//             });
+//         }
+//     });
+// }
+
 function createCovidMap(data) {
+    
+    var cases = L.geoJson(data, {
+        style: styleCovidCases,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h4>${feature['properties']['State/Territory']}</h4><hr><p>New Cases: ${feature['properties']['New Cases']}</p>
+                <p>Cases in Last 7 Days: ${feature['properties']['Cases in Last 7 Days']}</p><p>7-Day Cases Rate per 100000: ${feature['properties']['7-Day Cases Rate per 100000']}</p>
+                <p>Total Cases: ${feature['properties']['Total Cases']}</p><p>Case Rate per 100000: ${feature['properties']['Case Rate per 100000']}</p>`, 
+                {closeButton: false, offset: L.point(-20, -20)});
+            layer.on('mouseover', function() {
+                layer.openPopup();
+            });
+            layer.on('mouseout', function() {
+                layer.closePopup();
+            });
+            layer.on('click', function(event) {
+                map.fitBounds(event.target.getBounds());
+            });
+        }
+    });
+    var deaths = L.geoJson(data, {
+        style: styleCovidDeaths,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h4>${feature['properties']['State/Territory']}</h4><hr><p>New Deaths: ${feature['properties']['New Deaths']}</p>
+                <p>Deaths in Last 7 Days: ${feature['properties']['Deaths in Last 7 Days']}</p><p>7-Day Death Rate per 100000: ${feature['properties']['7-Day Death Rate per 100000']}</p>
+                <p>Total Deaths: ${feature['properties']['Total Deaths']}</p><p>Death Rate per 100000: ${feature['properties']['Death Rate per 100000']}</p>`, 
+                {closeButton: false, offset: L.point(-20, -20)});
+            layer.on('mouseover', function() {
+                layer.openPopup();
+            });
+            layer.on('mouseout', function() {
+                layer.closePopup();
+            });
+            layer.on('click', function(event) {
+                map.fitBounds(event.target.getBounds());
+            });
+        }
+    });
+    var overlayMaps = {
+        Cases: cases,
+        Deaths: deaths
+    };
+
     var map = L.map('map', {
         center: [37.8, -96],
         zoom: 4,
+        layers: [cases, deaths]
     });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -30,11 +115,8 @@ function createCovidMap(data) {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    L.geoJson(data, {
-        style: style_covid,
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup(`<h3>${feature['properties']['State/Territory']}</h3>`);
-        }
+    L.control.layers(null, overlayMaps, {
+        collapsed: false
     }).addTo(map);
 };
 
@@ -50,7 +132,7 @@ function createVaccineMap(data) {
     }).addTo(map);
 
     L.geoJson(data, {
-        style: style_vaccine,
+        style: styleVaccine,
         onEachFeature: function (feature, layer) {
             layer.bindPopup(`<h3>${feature['properties']['State/Territory']}</h3>`);
         }
@@ -58,17 +140,37 @@ function createVaccineMap(data) {
 };
 
 // Function to decide color for covid map
-function getCovidColor(x) {
+function getCovidCasesColor(x) {
     return x > 450 ? '#800026' :
            x > 300 ? '#E31A1C' :
            x > 150 ? '#FD8D3C' :
                      '#FED976';
 };
 
+// Function to decide color for covid map
+function getCovidDeathsColor(x) {
+    return x > 1.5 ? '#800026' :
+           x > 1 ? '#E31A1C' :
+           x > 0.5 ? '#FD8D3C' :
+                     '#FED976';
+};
+
 // Function to style the covid_map
-function style_covid(feature) {
+function styleCovidCases(feature) {
     return {
-        fillColor: getCovidColor(feature['properties']['7-Day Cases Rate per 100000']),
+        fillColor: getCovidCasesColor(feature['properties']['7-Day Cases Rate per 100000']),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    }; 
+}
+
+// Function to style the covid_map
+function styleCovidDeaths(feature) {
+    return {
+        fillColor: getCovidDeathsColor(feature['properties']['7-Day Death Rate per 100000']),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -86,7 +188,7 @@ function getVaccineColor(x) {
 };
 
 // Function to style the vaccine_map
-function style_vaccine(feature) {
+function styleVaccine(feature) {
     return {
         fillColor: getVaccineColor(feature['properties']['Doses Administered per 100k by State where Administered']),
         weight: 2,
